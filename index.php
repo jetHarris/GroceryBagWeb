c102a003a3d06e1fff6d1c2eb1173ffd7a2bf039
alexistodd08@gmail.com <BLACKc4t>
2016-09-24 01:17:23 -0400

    Revert "Layout Edits v1"
    This reverts commit 59e8082f7ac9229b0ed35861bc4781ce50456fca.

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <html>
 <head>
     <meta http-equiv="pragma" content="no-cache" />
-    <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet"/>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <script src="scripts/jquery-3.1.0.min.js"></script>
     <script src="scripts/bootstrap.min.js"></script>
     <script src="scripts/grocerybag.js"> </script>
-    <title>Grocery Bag</title>
+    <title>
+        Grocery Bag
+    </title>
 </head>
 <body onload="setup();">
-<nav class="navbar navbar-default navbar-fixed-top">
+<nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -19,7 +21,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a href="#"><img src="img/baglogo.png" id="logo" width="150"/></a>
+            <a class="navbar-brand" href="#">Project name</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
@@ -32,40 +34,34 @@
 </nav>
 
 <div class="container">
+<h1>Grocery Bag</h1>
+<img src="img/baglogo.jpg"/>
 <div>
-        <input type="text" id="filter" onkeyup="filterInput(this)" placeholder="Find"/>
+    <p>Filter:
+    <input type="text" id="filter" onkeyup="filterInput(this)"/></p>
 </div>
-<div class="table-container">
-
-
+<div>
 <?php require ("Connection.php");
 
 $sql= "SELECT * from grocerylist.items;";
 $check = mysqli_query($conn, $sql);
 
-$output =
-    '<table border="1">
-        <thead><tr id="not-header"><th class="id">ID</th>'.
-            '<th class="item">Item</th>'.
-            '<th class="price">Price</th>'.
-            '<th class="sale-price">Sale Price</th>'.
-            '<th class="checkmark">Sale</th>'.
-            '<th class="checkmarktax">GST</th>'.
-            '<th class="checkmarktax">PST</th>'.
-            '<th class="checkmarktax">HST</th><!--'.
-    '<th id="th-spacer"></th>--></tr>
-        </thead>
-    </table>
-<div id="itemBank" style="height:100%; overflow-y: auto; display: inline-block;">
-<table border="1" style="height: auto;">';
+$output = '<table border="1"><thead><tr><th class="id">ID</th>'.
+    '<th class="item">Item</th>'.
+    '<th class="price">Price</th>'.
+    '<th class="price">Sale Price</th>'.
+    '<th class="checkmark">On Sale</th>'.
+    '<th class="checkmarktax">GST</th>'.
+    '<th class="checkmarktax">PST</th>'.
+    '<th class="checkmarktax">HST</th></tr></thead></table><div id="itemBank" style="height:100px; overflow-y: auto; display: inline-block;"><table border="1">';
 
 while($row = mysqli_fetch_assoc($check)){
-    $output .='<tr>';
+    $output .='<tr class="highlight">';
     $output .= '<td class="id">'.$row['id'].'</td>';
     $output .= '<td class="item">'.$row['item_name'].'</td>';
     $output .= '<td class="price">$'.$row['price'].'</td>';
-    $output .= '<td class="sale-price">$'.$row['sale_price'].'</td>';
-    $output .= '<td  class="checkmark">'.($row['use_sale_price'] === '1' ? '&#9989;':'&#10008;').'</td>';
+    $output .= '<td class="price">$'.$row['sale_price'].'</td>';
+    $output .= '<td class="checkmark">'.($row['use_sale_price'] === '1' ? '&#9989;':'&#10008;').'</td>';
     $output .= '<td class="checkmarktax">'.($row['GST'] === '1' ? '&#9989;':'&#10008;').'</td>';
     $output .= '<td class="checkmarktax">'.($row['PST'] === '1' ? '&#9989;':'&#10008;').'</td>';
     $output .= '<td class="checkmarktax">'.($row['HST'] === '1' ? '&#9989;':'&#10008;').'</td>';
@@ -74,7 +70,6 @@ while($row = mysqli_fetch_assoc($check)){
 $output .='</tbody></table></div>';
 echo $output;
 ?>
-
 </div>
 </div>
 <div id="blank"></div>
